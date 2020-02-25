24c7daa675b1e5d07c8a5a8305ea9238da8fd003
RudolfStepanian <rudolf.stepanian@gmail.com>
2020-02-25 17:55:24 +0400

    third commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () =>{
+    return <header className='header'>
+        <img height='100px' width='180px' src='https://upload.wikimedia.org/wikipedia/commons/b/be/Lineage_OS_Logo.png' />
+      </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+    <div className='content'>
+        <div>
+            <img height='500px' width='600px' src='https://c8.alamy.com/comp/AXBEXR/stock-photograph-of-a-asian-teen-with-a-trumpet-to-her-ear-AXBEXR.jpg' />
+        </div>
+        <div>
+            ava + desc
+        </div>
+        <div>
+            <div>
+                posts
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Profile;
