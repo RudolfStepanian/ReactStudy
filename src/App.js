1346be322f9b95efc37c8ea26d3de28fce11bcb9
RudolfStepanian <rudolf.stepanian@gmail.com>
2020-02-26 23:31:29 +0400

    commite 7

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import Header from './components/Header/Header.jsx'
 import Nav from './components/Nav/Nav.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = (props) => {
-  return (
-    <div className='app-wrapper'>
-        <Header />
-        <Nav />
-        <Profile />
-        <Dialogs />
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Nav/>
+            <div className='app-wrapper-content'>
+                <Profile />
+                {/*<Dialogs/>*/}
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = () => {
     return (
-        <div className={cl.content}>
+        <div>
             <div>
                 <img height='500px' width='600px' src='https://c8.alamy.com/comp/AXBEXR/stock-photograph-of-a-asian-teen-with-a-trumpet-to-her-ear-AXBEXR.jpg' />
             </div>
